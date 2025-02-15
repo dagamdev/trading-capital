9d049037fe683081a93d319469a4ec525a3a98c6
gacub20ru <gacub20ru@gmail.com>
2025-02-15 20:56:01 +0000

    fix code

--- src/app/lib/index.ts
+++ src/app/lib/index.ts
@@ -1,19 +1,15 @@
-const target = {
-  finalBalance: 0,
-  profitPercentage: 0,
-  profit: 0
-}
+// const target = {
+//   finalBalance: 0,
+//   profitPercentage: 0,
+//   profit: 0
+// }
 
-const settings = {
-  balance: 100,
-  trades: 10,
-  ITMs: 5,
-  ROI: 80
-}
-
-function calculateTarget () {
-  
-}
+// const settings = {
+//   balance: 100,
+//   trades: 10,
+//   ITMs: 5,
+//   ROI: 80
+// }
 
 export function masaniello () {
   console.log(1.80^10)
